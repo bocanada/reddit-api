3b612a40ac86d38b90217b007abf35500b59bcec
Mauro M <24578415+Bocanada@users.noreply.github.com>
2023-06-25 00:52:37 -0300

    use Arc<str> instead of String

--- src/subreddit/submission/mod.rs
+++ src/subreddit/submission/mod.rs
@@ -1,6 +1,6 @@
 pub mod stream;
 
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use url::Url;
 
@@ -12,7 +12,7 @@ pub struct GalleryItem {
     /// The gallery item id.
     pub id: i64,
     /// The gallery item media id.
-    pub media_id: String,
+    pub media_id: Arc<str>,
 }
 
 /// [`GalleryData`] contains all items in a Reddit gallery.
@@ -40,14 +40,14 @@ pub struct MediaProperties {
 #[derive(Debug, serde::Deserialize)]
 pub struct MediaData {
     /// The media type.
-    pub e: Option<String>,
+    pub e: Option<Arc<str>>,
     /// The media id.
-    pub id: String,
+    pub id: Arc<str>,
     /// The media mime type.
     #[serde(rename = "m")]
-    pub mime: String,
+    pub mime: Arc<str>,
     /// The media status.
-    pub status: String,
+    pub status: Arc<str>,
     /// The biggest preview.
     #[serde(rename = "s")]
     pub biggest_preview: Option<MediaProperties>,
@@ -65,7 +65,7 @@ pub struct RedditVideo {
 pub struct Media {
     /// Where the media comes from.
     #[serde(rename = "type")]
-    pub media_type: Option<String>,
+    pub media_type: Option<Arc<str>>,
     /// The reddit video.
     pub reddit_video: Option<RedditVideo>,
 }
@@ -73,11 +73,11 @@ pub struct Media {
 #[derive(Debug, serde::Deserialize)]
 pub struct Submission {
     /// The author of this post.
-    pub author: String,
+    pub author: Arc<str>,
     /// The perma link to this post.
     pub permalink: RedditUrl,
     /// The base36 internal Reddit identifier for this post, e.g. 2qpqw.
-    pub id: String,
+    pub id: Arc<str>,
     /// The full 'Thing ID', consisting of a 'kind' and a base-36 identifier. The valid kinds are:
     /// - t1_ - Comment
     /// - t2_ - Account
@@ -86,13 +86,13 @@ pub struct Submission {
     /// - t5_ - Subreddit
     /// - t6_ - Award
     /// - t8_ - PromoCampaign
-    pub name: String,
+    pub name: Arc<str>,
     /// The linked URL, if this is a link post.
     pub url: Option<RedditUrl>,
     /// The title of the post.
-    pub title: String,
+    pub title: Arc<str>,
     /// The subreddit that this submission was posted in (not including `/r/`)
-    pub subreddit: String,
+    pub subreddit: Arc<str>,
     /// The items of a gallery.
     pub gallery_data: Option<Gallery>,
     /// The media metadata.
