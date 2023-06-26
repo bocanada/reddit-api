761a0dd8871697e14046cbd0aaee94ecdb17abbe
Mauro M <24578415+Bocanada@users.noreply.github.com>
2023-06-26 02:44:47 -0300

    fix error message

--- src/errors.rs
+++ src/errors.rs
@@ -2,7 +2,7 @@
 pub enum Error {
     #[error("reqwest error: {0}")]
     Request(#[from] reqwest::Error),
-    #[error("reqwest error: {0}")]
+    #[error("reddit error: {0}")]
     Reddit(#[from] RedditError),
     #[error("authentication error: {0}")]
     AuthError(#[from] crate::auth::Error),
