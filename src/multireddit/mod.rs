a62090cef645963982b5801d26886ca1c4938e69
Mauro M <24578415+Bocanada@users.noreply.github.com>
2023-07-25 18:55:40 -0300

    add stream for MultiReddit

--- src/multireddit/mod.rs
+++ src/multireddit/mod.rs
@@ -1,13 +1,22 @@
-pub mod response;
+pub(crate) mod response;
 
 use url::Url;
 
 use crate::response::RedditUrl;
-use crate::subreddit::Subreddit;
+#[cfg(feature = "stream")]
+use crate::subreddit::submission::Submission;
 use std::path::PathBuf;
 use std::sync::Arc;
+use std::time::Duration;
 
 use crate::auth::Authenticator;
+#[cfg(feature = "stream")]
+use crate::subreddit::feed;
+use crate::subreddit::Subreddit;
+#[cfg(feature = "stream")]
+use futures_util::{stream::SelectAll, Stream};
+#[cfg(feature = "stream")]
+use std::ops::RangeBounds;
 
 #[derive(Clone, Debug)]
 pub struct Multireddit<A: Authenticator> {
@@ -25,10 +34,33 @@ pub struct Multireddit<A: Authenticator> {
     pub owner: Arc<str>,
     pub owner_id: Arc<str>,
     pub path: RedditUrl,
-    pub subreddits: Arc<[Subreddit<A>]>,
+    pub subreddits: Vec<Subreddit<A>>,
 }
 
-// TODO: Implement stream for MultiReddit
+#[cfg(feature = "stream")]
+type MultiStream<T> = SelectAll<T>;
+
+impl<A: Authenticator> Multireddit<A> {
+    /// Creates a new [`Stream`] of [`Submission`].
+    #[cfg(feature = "stream")]
+    #[doc(cfg(feature = "stream"))]
+    pub fn stream(
+        self,
+        sort: feed::Sort,
+        every: Duration,
+        spread: impl RangeBounds<u64> + Clone,
+    ) -> MultiStream<impl Stream<Item = crate::Result<Submission>>> {
+        use futures_util::stream::select_all;
+        use nanorand::{Rng, WyRand};
+        use tokio::time::interval;
+        let mut rng = WyRand::new();
+
+        select_all(self.subreddits.into_iter().map(|sub| {
+            let every = interval(every + Duration::from_secs(rng.generate_range(spread.clone())));
+            sub.stream_inner(sort, every, rng.generate())
+        }))
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct MultiPath {
@@ -64,9 +96,10 @@ impl From<MultiPath> for PathBuf {
 
 #[cfg(test)]
 mod test {
-    use std::path::PathBuf;
+    use std::{path::PathBuf, time::Duration};
 
     use dotenv::{dotenv, var};
+    use futures_util::StreamExt;
 
     use super::MultiPath;
     use crate::Client;
@@ -92,5 +125,34 @@ mod test {
             .multi(MultiPath::new("singshredcode", "animal_subbies"))
             .await;
         assert!(multi.is_ok());
+    }
+
+    #[tokio::test]
+    async fn anon_multi_stream() {
+        dotenv().unwrap();
+        let pkg_name = env!("CARGO_PKG_NAME");
+        let username = var("REDDIT_USERNAME").unwrap();
+
+        let client = Client::new(&format!("{pkg_name} (by u/{username})"));
+
+        let multi = client
+            .multi(MultiPath::new("singshredcode", "animal_subbies"))
+            .await;
+        assert!(multi.is_ok());
+        let multi = multi.unwrap();
+        let n = multi
+            .stream(
+                crate::subreddit::feed::Sort::New,
+                Duration::from_secs(120),
+                20..=60,
+            )
+            .take(200)
+            .take_while(|r| futures_util::future::ready(r.is_ok()))
+            .fold(0, |state, next| async move {
+                next.map(|_| 1).unwrap_or(0) + state
+            })
+            .await;
+
+        assert_eq!(n, 200);
     }
 }
